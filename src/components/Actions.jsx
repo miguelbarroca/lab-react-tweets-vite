3e033660767a23a951dd2c7c483608ee93a7de89
Miguel Barroca <miguelbarroca@protonmail.com>
2023-02-20 16:59:51 +0000

    missing map method

---
+++ src/components/Actions.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Actions() {
+  return (
+    <div>
+        <i className='far fa-comment'></i>
+        <i className='fas fa-retweet'></i>
+        <i className='far fa-heart'></i>
+        <i classname='fas fa-share'></i>
+    </div>
+  )
+}   
+
+export default Actions
